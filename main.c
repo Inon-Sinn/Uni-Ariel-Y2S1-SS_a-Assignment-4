d317f483a1632bcef181a392f8c07efea718596b
Inon Sinn <inon.sinn@gmail.com>
2021-12-30 18:55:16 +0200

    savepoint

--- main.c
+++ main.c
@@ -8,12 +8,19 @@ int main(int argc, char const *argv[])
     scanf("\n%c",&input);
     while (input == 'A' || input == 'B' || input =='D' || input=='S'|| input == 'T')//TODO check for better ways
     {
-        if (input == 'A')//Remove the former Graph
+        if (input == 'A')
         {
+            //TODO Remove the former Graph
+            int amount_of_Nodes;
+            scanf("\n%d",&amount_of_Nodes);
+            printf("The amount of Nodes is %d\n",amount_of_Nodes);
             scanf("\n%c",&input);
             while (input == 'n')
             {
-                printf("New Node");
+                int node_id;
+                scanf("\n%d",&node_id);
+                printf("The new Node id is %d\n",node_id);
+                // Find the node 
                 scanf("\n%c",&input);
             }
             
@@ -21,10 +28,14 @@ int main(int argc, char const *argv[])
         if (input == 'B')
         {
             printf("You have entered B\n");
+            int node_id;
+            scanf("\n%d",&node_id);
+            printf("The new Node id is %d\n",node_id);
         }
         if (input == 'D')
         {
             printf("You have entered D\n");
+
         }
         if (input == 'S')
         {
