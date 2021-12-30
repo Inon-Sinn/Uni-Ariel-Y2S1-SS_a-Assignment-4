6d0ab38f01b6483d8900ffa863f5d91ca231f533
Inon Sinn <inon.sinn@gmail.com>
2021-12-30 18:31:13 +0200

    Finished the Base of the Main

--- main.c
+++ main.c
@@ -3,6 +3,40 @@
 
 int main(int argc, char const *argv[])
 {
-    printf("Makefile check");
+    printf("Please Enter your input");
+    char input;
+    scanf("\n%c",&input);
+    while (input == 'A' || input == 'B' || input =='D' || input=='S'|| input == 'T')//TODO check for better ways
+    {
+        if (input == 'A')//Remove the former Graph
+        {
+            scanf("\n%c",&input);
+            while (input == 'n')
+            {
+                printf("New Node");
+                scanf("\n%c",&input);
+            }
+            
+        }
+        if (input == 'B')
+        {
+            printf("You have entered B\n");
+        }
+        if (input == 'D')
+        {
+            printf("You have entered D\n");
+        }
+        if (input == 'S')
+        {
+            printf("You have entered S\n");
+        }
+        if (input == 'T')
+        {
+            printf("You have entered T\n");
+        }
+        scanf("\n%c",&input);
+    }
+    printf("End");
+
     return 0;
 }
