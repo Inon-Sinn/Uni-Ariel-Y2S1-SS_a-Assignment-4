5a2496d11dd79f345696c145430e0b46e4b761c4
Inon Sinn <inon.sinn@gmail.com>
2022-01-02 23:18:03 +0200

    Start of Dijkstra, found a bug in B

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "graph.h"
+#include "algo.h"
+
+//Finds the shortest path using the dijkstra Algorithm
+int dijkstra(pnode *head,int amount_of_Nodes, int src, int dest);
 
 int main(int argc, char const *argv[])
 {
@@ -35,8 +39,9 @@ int main(int argc, char const *argv[])
                 scanf("%c",&input);
             }
         }
-        if (input == 'B')
+        if (input == 'B')//TODO not finished
         {
+            //Remove the Node if it exists
             // Find the node
             scanf("%d",&node_id);
             pnode currentNode = getNode(&head,node_id);
@@ -52,11 +57,17 @@ int main(int argc, char const *argv[])
             int node_id;
             scanf("%d",&node_id);
             removeNode(&head,node_id);
+            amount_of_Nodes--;
             scanf("%c",&input);
         }
         if (input == 'S')
         {
-            printf("You have entered S\n");
+            //Collect the source and destination node
+            scanf("%d",&node_id);
+            scanf("%d",&dest);
+            //find the shortest distance
+            dijkstra(&head, amount_of_Nodes,node_id,dest);
+            scanf("%c",&input);
         }
         if (input == 'T')
         {
@@ -69,20 +80,28 @@ int main(int argc, char const *argv[])
     return 0;
 }
 
-
-
-
-//     pnode head = NULL;
-//     build_graph_cmd(&head,5);
-//     printGraph_cmd(head);
-//     printf("-------\n");
-//     pnode gotNode = getNode(&head,4);
-//     addEdge(gotNode,2,10, &head);
-//     addEdge(gotNode,1,20, &head);
-//     printGraph_cmd(head);
-//     printf("-------\n");
-//     removeNode(&head,2);
-//     printGraph_cmd(head);
-//     printf("-------\n");
-//    build_graph_cmd(&head,3);
-//     printGraph_cmd(head);
+int dijkstra(pnode *head,int amount_of_Nodes,  int src, int dest){
+    //Create the array that save the distance and the Queue
+    int *d = (int*)malloc(sizeof(int)*amount_of_Nodes);
+    int *Queue = (int*)malloc(sizeof(int)*amount_of_Nodes);
+    int start = 0,end =0;
+    // Check if memory was allocated
+    if (d==NULL || Queue ==NULL)
+    {
+        printf("Memory Not aloocated");
+        return -1;
+    }
+    //initlize the array
+    int i;
+    for (i = 0; i < amount_of_Nodes; i++)
+        d[i] = -1;
+    //Enter the first node into the array and Queue;
+    d[src] = 0;
+    Queue[start] = src;
+    end++;
+    while (start<end)
+    {
+        printf("hi");
+    }
+    return -1;
+}
